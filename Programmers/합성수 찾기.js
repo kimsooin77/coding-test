83e9a0bd70c17ea45ec610507e0b65c0ace1a826
kimsooin77 <sm6784@naver.com>
2023-10-06 12:32:10 +0900

    합성수 찾기, 2차원으로 만들기

---
+++ Programmers/합성수 찾기.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var answer = 0;
+    for(let i = 1; i<=n; i++) {
+        let arr = [];
+        for(let j = 1; j <= i; j++) {
+            if(i%j === 0) arr.push(j)
+        }
+        if(arr.length >=3) answer++;
+    }
+    return answer;
+}
